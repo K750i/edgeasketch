62f1592df80d1dac20113c8a3a00a4c41c3f58e2
K750i <munkin80@gmail.com>
2023-02-24 11:21:24 +0800

    Paint with 0.1 increment in opacity

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 
 const GRIDWIDTH = '32';
 const grid = document.querySelector('.grid');
-const createGrid = function (qty) {
-    qty = 32;
+const size = document.querySelector('#size').value;
+const createGrid = function (gridSize) {
+    gridSize = parseInt(size);
     const fragment = document.createDocumentFragment();
-    const boxSize = 100 / qty;
+    const boxSize = 100 / gridSize;
 
-    for (let i = 0; i < qty * qty; i++) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
         const div = document.createElement('div');
         div.className = 'box';
         div.style.width = `${boxSize}%`;
@@ -18,10 +19,18 @@ const createGrid = function (qty) {
     grid.style.height = `${GRIDWIDTH}vw`;
     grid.appendChild(fragment);
 };
-
+let color, curOpacity;
+let [r, g, b, a] = [0, 0, 0, 0.1];
 window.addEventListener('DOMContentLoaded', createGrid);
+
 grid.addEventListener('mouseover', e => {
     if (e.target.classList.contains('box')) {
-        e.target.style.backgroundColor = 'darkslateblue';
+        color = window.getComputedStyle(e.target).backgroundColor;
+        if (color.includes('rgba')) {
+            curOpacity = parseFloat(color.slice(color.lastIndexOf(',') + 1, -1).trim())
+        } else {
+            curOpacity = 1;
+        }
+        e.target.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${Math.min(a + curOpacity, 1)})`;
     }
 });
