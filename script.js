7c84ab89f5b0827433067e85fac704681250eb66
K750i <munkin80@gmail.com>
2023-02-26 10:03:21 +0800

    Add ability to change grid size and random colors

--- script.js
+++ script.js
@@ -1,36 +1,86 @@
 'use strict';
 
-const GRIDWIDTH = '32';
-const grid = document.querySelector('.grid');
-const size = document.querySelector('#size').value;
-const createGrid = function (gridSize) {
-    gridSize = parseInt(size);
-    const fragment = document.createDocumentFragment();
-    const boxSize = 100 / gridSize;
-
-    for (let i = 0; i < gridSize * gridSize; i++) {
-        const div = document.createElement('div');
-        div.className = 'box';
-        div.style.width = `${boxSize}%`;
-        div.style.height = `${boxSize}%`;
-        fragment.appendChild(div);
-    }
-    grid.style.width = `${GRIDWIDTH}vw`;
-    grid.style.height = `${GRIDWIDTH}vw`;
-    grid.appendChild(fragment);
-};
 let color, curOpacity;
 let [r, g, b, a] = [0, 0, 0, 0.1];
-window.addEventListener('DOMContentLoaded', createGrid);
+const GRIDSIZE = '32';
+const grid = document.querySelector('.grid');
+const sizeInput = document.querySelector('#size');
+const checkbox = document.querySelector('#chk')
+const colorText = document.querySelector('.color')
+const getRandomColor = function () {
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+};
+const createGrid = function () {
+    let gridsize = parseInt(sizeInput.value);
+    if (Number.isNaN(gridsize)) {
+        alert('Invalid number.');
+        return;
+    }
+    if (gridsize < 1 || gridsize > 100) {
+        alert('Please enter a size between 1 and 100.');
+        return;
+    }
+    const fragment = document.createDocumentFragment();
+    const boxsize = 100 / gridsize;
+
+    for (let i = 0; i < gridsize * gridsize; i++) {
+        const div = document.createElement('div');
+        div.className = 'box';
+        div.style.width = `${boxsize}%`;
+        div.style.height = `${boxsize}%`;
+        fragment.appendChild(div);
+    }
+    grid.style.width = `${GRIDSIZE}vw`;
+    grid.style.height = `${GRIDSIZE}vw`;
+    grid.appendChild(fragment);
+};
+const changeGrid = function () {
+    grid.innerHTML = "";
+    createGrid();
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+    sizeInput.value = 16;
+    createGrid();
+    if (checkbox.checked) {
+        colorText.classList.add('selected');
+        getRandomColor();
+    }
+    else {
+        colorText.classList.remove('selected');
+        [r, g, b] = [0, 0, 0];
+    }
+});
+
+sizeInput.addEventListener('change', changeGrid);
 
 grid.addEventListener('mouseover', e => {
     if (e.target.classList.contains('box')) {
         color = window.getComputedStyle(e.target).backgroundColor;
         if (color.includes('rgba')) {
-            curOpacity = parseFloat(color.slice(color.lastIndexOf(',') + 1, -1).trim())
+            curOpacity = parseFloat(color.slice(color.lastIndexOf(',') + 1, -1));
         } else {
             curOpacity = 1;
         }
         e.target.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${Math.min(a + curOpacity, 1)})`;
     }
 });
+
+grid.addEventListener('mouseleave', function () {
+    if (checkbox.checked) {
+        getRandomColor()
+    }
+});
+
+checkbox.addEventListener('change', function () {
+    if (this.checked) {
+        colorText.classList.add('selected');
+        getRandomColor();
+    }
+    else {
+        colorText.classList.remove('selected');
+        [r, g, b] = [0, 0, 0];
+    }
+})
